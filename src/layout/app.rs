f2c7898c6f1c303e20552832c378d633e0c942c4
monomadic <monomadic.crypto>
2022-11-23 03:07:17 +0800

    refactor: move app into app.rs

---
+++ src/layout/app.rs
@@ -0,0 +1,16 @@
+use super::header::Header;
+use crate::components::NetworkMismatch;
+use yew::prelude::*;
+use yew_ethereum_provider::{EthereumContextProvider, UseEthereumHandle};
+
+#[function_component]
+pub(crate) fn App() -> Html {
+    html! {
+        <EthereumContextProvider>
+            <Header />
+            <main class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 text-center">
+                <NetworkMismatch />
+            </main>
+        </EthereumContextProvider>
+    }
+}
--- src/layout/mod.rs
+++ src/layout/mod.rs
@@ -1,17 +1,3 @@
-use crate::components::NetworkMismatch;
-use yew::prelude::*;
-use yew_ethereum_provider::EthereumContextProvider;
-
-pub(crate) mod header;
-
-#[function_component]
-pub(crate) fn App() -> Html {
-    html! {
-        <EthereumContextProvider>
-            <header::Header />
-            <main class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 text-center">
-                <NetworkMismatch />
-            </main>
-        </EthereumContextProvider>
-    }
-}
+mod app;
+mod header;
+pub(crate) use app::App;
